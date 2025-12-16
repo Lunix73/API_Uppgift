bc1b018581f6a3d4bb9633e6702e01c2fba81d70
Lunix73 <linus.v.leitz@gmail.com>
2025-12-16 11:47:39 +0100

    Slutlig commit

--- script.sql
+++ script.sql
@@ -1,14 +1,3 @@
-/*CREATE DATABASE nodejs_rest_api;
-USE nodejs_rest_api;
-
-LOAD DATA LOCAL INFILE '/home/linus/demodata.csv'  INTO TABLE
-Varor FIELDS TERMINATED BY ',' IGNORE 1 LINES (Month, Category, Sales);
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);*/
 
 CREATE DATABASE industrial_products;
 USE industrial_products;
